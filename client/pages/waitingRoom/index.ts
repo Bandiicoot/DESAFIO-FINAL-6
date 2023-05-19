44477b76d053d1790c2af9d4d42bbbe108b18f54
unknown <fa-ndo@hotmail.com>
2023-05-19 18:10:23 -0300

    grid

--- client/pages/waitingRoom/index.ts
+++ client/pages/waitingRoom/index.ts
@@ -208,7 +208,7 @@ customElements.define(
       buttonListoEl.addEventListener("click", () => {
         console.log("Estoy tocando el boton listo");
         buttonListoEl.textContent = "Esperando";
-        juanjoCagando.style.display = "block";
+        juanjoCagando.style.display = "grid";
       });
 
       const goBackButtonEl = this.shadow.querySelector(".volver");
