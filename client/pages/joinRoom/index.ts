e674e6d5e60a131b18e0bd13bb8165c5ef7058db
unknown <fa-ndo@hotmail.com>
2023-05-29 17:38:42 -0300

    console.log

--- client/pages/joinRoom/index.ts
+++ client/pages/joinRoom/index.ts
@@ -14,6 +14,7 @@ customElements.define(
       formEl.addEventListener("submit", (e) => {
         e.preventDefault();
         const formData = new FormData(e.target as HTMLFormElement);
+        console.log(formData);
         const objeto = Object.fromEntries(formData.entries());
         state.askRTDBroom(objeto.inputRoomId);
       });
